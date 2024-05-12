3fabefc93af49c535dcfc80da00989a0ef76e440
Harrinson Toledo Gonzalez <75257608+HarrinsonToledo@users.noreply.github.com>
2024-05-12 14:40:04 -0500

    clean userinterface

--- Frontend/src/app/components/user-interface/user-interface.component.ts
+++ Frontend/src/app/components/user-interface/user-interface.component.ts
@@ -17,16 +17,6 @@ import { SidebarComponent } from '../sidebar/sidebar.component';
   styleUrl: './user-interface.component.css',
 })
 export class UserInterfaceComponent {
-  public w: string = 'w-20';
-  public view: boolean = false;
-  public arrow: string = 'arrowBarInv.png';
-  public location: string = 'Location IconWhite.png';
-  public car: string = 'CarIcon.png';
-  public histo: string = 'HistorialIcon.png';
-  public data: string = 'DataIcon.png';
-  public log: string = 'LogOutIcon.png';
-  public logo: string = '';
-  public logoStyle: string = 'w-16 h-16 my-10 rounded-full';
 
   public cities!: Array<infoCities>;
   public parking!: Array<infoParking>;
@@ -62,15 +52,6 @@ export class UserInterfaceComponent {
       },
       error: (Error) => {},
     });
-  }
-
-  ngDoCheck() {
-    this.w = this.view ? 'min_oculy:w-3/12 w-4/12' : 'w-20';
-    this.arrow = this.view ? 'arrowBar.png' : 'arrowBarInv.png';
-    this.logoStyle = this.view
-      ? 'w-3/5 my-10 rounded-xl mx-auto p-1 border-2 border-white'
-      : 'w-12 my-10 rounded-full';
-    this.logo = this.view ? 'LOGOBL.png' : 'LOGOLocation.png';
   }
 
   filterCity(city: Event) {
